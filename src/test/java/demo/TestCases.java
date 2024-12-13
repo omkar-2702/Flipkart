2cf18cc752af8a4ca508462114824a0ee364a02f
omkarbhagwatwar@gmail.com <omkarbhagwatwar@gmail.com>
2024-12-13 23:00:15 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,19 +1,86 @@
+// package demo;
+
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeDriverService;
+// import org.openqa.selenium.chrome.ChromeOptions;
+// import org.openqa.selenium.logging.LogType;
+// import org.openqa.selenium.logging.LoggingPreferences;
+// import org.testng.annotations.AfterTest;
+// import org.testng.annotations.BeforeTest;
+// import org.testng.annotations.Test;
+
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.logging.Level;
+
+// // import io.github.bonigarcia.wdm.WebDriverManager;
+// import demo.wrappers.Wrappers;
+
+// public class TestCases {
+//     ChromeDriver driver;
+
+//     /*
+//      * TODO: Write your tests here with testng @Test annotation. 
+//      * Follow `testCase01` `testCase02`... format or what is provided in instructions
+//      */
+
+//     /*
+//      * Do not change the provided methods unless necessary, they will help in automation and assessment
+//      */
+//     @BeforeTest
+//     public void startBrowser()
+//     {
+//         System.setProperty("java.util.logging.config.file", "logging.properties");
+
+//         // NOT NEEDED FOR SELENIUM MANAGER
+//         // WebDriverManager.chromedriver().timeout(30).setup();
+
+//         ChromeOptions options = new ChromeOptions();
+//         LoggingPreferences logs = new LoggingPreferences();
+
+//         logs.enable(LogType.BROWSER, Level.ALL);
+//         logs.enable(LogType.DRIVER, Level.ALL);
+//         options.setCapability("goog:loggingPrefs", logs);
+//         options.addArguments("--remote-allow-origins=*");
+
+//         System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+
+//         driver = new ChromeDriver(options);
+
+//         driver.manage().window().maximize();
+//     }
+
+//     @AfterTest
+//     public void endTest()
+//     {
+//         driver.close();
+//         driver.quit();
+
+//     }
+// }
+
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
-
 
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
@@ -22,17 +89,16 @@ public class TestCases {
     ChromeDriver driver;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow testCase01 testCase02... format or what is provided in instructions
      */
 
-     
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -46,16 +112,85 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
     }
 
+    @Test
+    public void testCase01() throws InterruptedException {
+        System.out.println("Start Test case 01");
+        driver.get("https://www.flipkart.com/");
+
+        WebElement inputBox = driver.findElement(By.xpath("//input[@title = 'Search for Products, Brands and More']"));
+        inputBox.click();
+        inputBox.clear();
+        inputBox.sendKeys("Washing Machine");
+        inputBox.sendKeys(Keys.ENTER);
+
+        Thread.sleep(3000);
+
+        Wrappers.clickElement(driver, By.xpath("//div[text()='Popularity']"));
+        Thread.sleep(2000);
+
+        List<WebElement> starRatingElements = driver
+                .findElements(By.xpath("//span[contains(@id , 'productRating')]/div"));
+        for (WebElement ratingElement : starRatingElements) {
+            double rating = Double.parseDouble(ratingElement.getText());
+            if (rating <= 4.0) {
+                System.out.println(ratingElement.getText());
+            }
+        }
+        System.out.println("End Test case 01");
+
+    }
+
+    @Test
+    public void testCase02() throws InterruptedException {
+        System.out.println("Start Test case 02");
+        driver.get("https://www.flipkart.com/");
+        Thread.sleep(3000);
+
+        // click on cancel icon
+        Wrappers.clickElement(driver, By.xpath("//span[@role='button']"));
+        WebElement inputBox = driver.findElement(By.xpath("//input[@title = 'Search for Products, Brands and More']"));
+
+        inputBox.click();
+        inputBox.clear();
+        inputBox.sendKeys("iPhone");
+        inputBox.sendKeys(Keys.ENTER);
+        Thread.sleep(3000);
+
+        Boolean status = Wrappers.printTitleAndDiscount(driver, By.xpath("//div[contains(@class, 'yKfJKb')]"), 17);
+        Assert.assertTrue(status);
+        System.out.println("End Test case 02");
+
+    }
+
+    @Test
+    public void testCase03() throws InterruptedException {
+        System.out.println("Start Test case 03");
+        driver.get("https://www.flipkart.com/");
+        // click on cancel icon
+        Wrappers.clickElement(driver, By.xpath("//span[@role='button']"));
+        Wrappers.searchProduct(driver, By.xpath("//input[@title = 'Search for Products, Brands and More']"),
+                "Coffee Mug");
+        Thread.sleep(3000);
+
+        Wrappers.clickElement(driver, By.xpath("//div[text() = '4★ & above']"));
+        Thread.sleep(3000);
+
+        Boolean status = Wrappers.printTitleAndImageUrl(driver,
+                By.xpath("//div[@class = 'slAVV4']//span[@class = 'Wphh3N']"));
+        Assert.assertTrue(status);
+        System.out.println("End Test case 03");
+
+    }
+
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
 
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,20 +3,174 @@ package demo.wrappers;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.text.NumberFormat;
 import java.time.Duration;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+    public static void searchProduct(WebDriver driver, By locator, String product) {
+        System.out.println("sending keys");
+        try {
+            // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            // wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+            WebElement inputBox = driver.findElement(locator);
+            inputBox.click();
+            inputBox.clear();
+            inputBox.sendKeys(product);
+            inputBox.sendKeys(Keys.ENTER);
+
+        } catch (Exception e) {
+            System.out.println("Exception occurred!" + e.getMessage());
+
+        }
+
+    }
+
+    public static void clickElement(WebDriver driver, By locator) {
+        System.out.println("Clicking on element");
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.elementToBeClickable(locator));
+
+            WebElement clickableElement = driver.findElement(locator);
+            clickableElement.click();
+
+        } catch (Exception e) {
+            System.out.println("Exception occurred!" + e.getMessage());
+
+        }
+    }
+
+    public static Boolean searchStarRatingAndPrintCount(WebDriver driver, By locator, double starRating) {
+
+        int washingMachineCount = 0;
+        Boolean success = false;
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+            wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+            List<WebElement> starRatingElements = driver.findElements(locator);
+            for (WebElement rating : starRatingElements) {
+                if (Double.parseDouble(rating.getText()) <= starRating) {
+                    washingMachineCount++;
+                }
+                System.out.println("Count of washing machines with rating less than or equal to " + starRating + ":"
+                        + washingMachineCount);
+                success = true;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Exception occurred!" + e.getMessage());
+            e.printStackTrace();
+            success = false;
+        }
+
+        return success;
+    }
+
+    public static Boolean printTitleAndDiscount(WebDriver driver, By locator, int discount) {
+        Boolean success = false;
+        try {
+            Thread.sleep(3000);
+            List<WebElement> productList = driver.findElements(locator);
+            HashMap<String, String> TitlediscountMap = new HashMap<>();
+            for (WebElement product : productList) {
+                String discountPercent = product.findElement(By.xpath("//div[@class= 'UkUFwK']/span")).getText();
+                Thread.sleep(3000);
+                System.out.println("Printing discount percent " + discountPercent);
+                int discountValue = Integer.parseInt(discountPercent.replaceAll("[^\\d]", ""));
+                if (discountValue > discount) {
+                    String title = product.findElement(By.xpath(".//div[@class = 'KzDlHZ']")).getText();
+                    TitlediscountMap.put(discountPercent, title);
+                    System.out.println("Discount map " + TitlediscountMap);
+
+                }
+            }
+
+            System.out.println("Checking success status first time " + success);
+
+            for (Map.Entry<String, String> Titlediscounts : TitlediscountMap.entrySet()) {
+                System.out.println("Iphone discount percentage is:" + Titlediscounts.getKey() + "and its title is:"
+                        + Titlediscounts.getValue());
+
+            }
+            success = true;
+            System.out.println("Checking success status second time " + success);
+
+        } catch (Exception e) {
+            System.out.println("Exception occurred!");
+            e.printStackTrace();
+            success = false;
+
+        }
+
+        return success;
+
+    }
+
+    public static Boolean printTitleAndImageUrl(WebDriver driver, By locator) {
+
+        Boolean success;
+        try {
+            List<WebElement> userReviewElements = driver.findElements(locator);
+            Set<Integer> userReviewSet = new HashSet<>();
+            for (WebElement userReview : userReviewElements) {
+                int userReviews = Integer.parseInt(userReview.getText().replaceAll("[^\\d]", ""));
+                userReviewSet.add(userReviews);
+            }
+
+            List<Integer> userReviewCountList = new ArrayList<>(userReviewSet);
+            Collections.sort(userReviewCountList, Collections.reverseOrder());
+            System.out.println(userReviewCountList);
+            NumberFormat numberFormat = NumberFormat.getInstance(Locale.US);
+            LinkedHashMap<String, String> productDetailsMap = new LinkedHashMap<>();
+            for (int i = 0; i < 5; i++) {
+                String formattedUserReviewCount = "(" + numberFormat.format(userReviewCountList.get(i)) + ")";
+                String productTitle = driver.findElement(By.xpath("//div[@class = 'slAVV4']//span[contains(text() , '"
+                        + formattedUserReviewCount + "')]/../../a[@class = 'wjcEIp']")).getText();
+                String productImageUrl = driver
+                        .findElement(By.xpath("//div[@class = 'slAVV4']//span[contains(text() , '"
+                                + formattedUserReviewCount + "')]/../..//img[@class = 'DByuf4']"))
+                        .getAttribute("src");
+                String highestReviewCountAndProductTitle = String.valueOf(
+                        (i + 1) + "highest review count" + formattedUserReviewCount + "Title is" + productTitle);
+                productDetailsMap.put(highestReviewCountAndProductTitle, productImageUrl);
+            }
+
+            // print title and image url of coffee mug
+            for (Map.Entry<String, String> productDetails : productDetailsMap.entrySet()) {
+
+                System.out.println(productDetails.getKey() + "and product image url is" + productDetails.getValue());
+            }
+            success = true;
+
+        } catch (Exception e) {
+            System.out.println("Exception occurred!");
+            e.printStackTrace();
+            success = false;
+
+        }
+
+        return success;
+    }
+
 }
